3e345657e5dc8ec68cecd575caadd39344b6feec
genekomarov <gene.komarov@gmail.com>
2020-03-30 15:10:56 +0500

    Урок 18 Создание Dialogs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Prorfile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
@@ -10,7 +11,10 @@ const App = () => {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Prorfile/>
+            {/*<Prorfile/>*/}
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+            </div>
         </div>
     );
 };
