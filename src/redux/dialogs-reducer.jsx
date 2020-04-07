cd886876473b6400c16196b2b2e8fe5a66ed01b7
genekomarov <gene.komarov@gmail.com>
2020-04-07 22:58:02 +0500

    Урок 46. Копия состояния в Reducer
    stateCopy = {...state}
    stateCopy.posts = {...state.posts}

--- src/redux/dialogs-reducer.jsx
+++ src/redux/dialogs-reducer.jsx
@@ -20,17 +20,20 @@ let initialState = {
 
 const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_MESSAGE:
+        case ADD_MESSAGE: {
             let newMessage = {
                 id: 2,
                 message: state.newMessageText
             };
-            state.messages.push(newMessage);
-            state.newMessageText ='';
-            return state;
-        case CHANGE_NEW_MESSAGE_TEXT:
-            state.newMessageText = action.newMessageText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.messages = [...state.messages]
+            stateCopy.messages.push(newMessage);
+            stateCopy.newMessageText ='';
+            return stateCopy; }
+        case CHANGE_NEW_MESSAGE_TEXT: {
+            let stateCopy = {...state};
+            stateCopy.newMessageText = action.newMessageText;
+            return stateCopy; }
         default: return state;
     }
 };
--- src/redux/profile-reducer.jsx
+++ src/redux/profile-reducer.jsx
@@ -12,18 +12,21 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 2,
                 post: state.newPostText,
                 likes: 0
             };
-            state.posts.push(newPost);
-            state.newPostText='';
-            return state;
-        case CHANGE_NEW_POST_TEXT:
-            state.newPostText = action.newPostText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText='';
+            return stateCopy; }
+        case CHANGE_NEW_POST_TEXT: {
+            let stateCopy = {...state}
+            stateCopy.newPostText = action.newPostText;
+            return stateCopy; }
         default: return state;
     }
 };
