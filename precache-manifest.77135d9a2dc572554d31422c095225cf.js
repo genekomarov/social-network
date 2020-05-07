eb2a3773971166bd9a308b2e98ca8ab1ed49cb20
genekomarov <gene.komarov@gmail.com>
2020-05-07 21:32:38 +0500

    Updates

---
+++ precache-manifest.77135d9a2dc572554d31422c095225cf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaf5378237406e24582c13e81c1c562",
+    "url": "/React-Kamasutra/index.html"
+  },
+  {
+    "revision": "77b36393ca011fe18234",
+    "url": "/React-Kamasutra/static/css/3.d5c179fe.chunk.css"
+  },
+  {
+    "revision": "6f4c53e9d37ff0b46dc3",
+    "url": "/React-Kamasutra/static/css/4.9fe74602.chunk.css"
+  },
+  {
+    "revision": "745fe3f6a3da892eb39a",
+    "url": "/React-Kamasutra/static/css/5.10d3189d.chunk.css"
+  },
+  {
+    "revision": "50272ab09f0332368bf2",
+    "url": "/React-Kamasutra/static/css/main.76e06dac.chunk.css"
+  },
+  {
+    "revision": "d19851b4d1a80a65f855",
+    "url": "/React-Kamasutra/static/js/2.8abf4d0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Kamasutra/static/js/2.8abf4d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b36393ca011fe18234",
+    "url": "/React-Kamasutra/static/js/3.7bdd3b36.chunk.js"
+  },
+  {
+    "revision": "6f4c53e9d37ff0b46dc3",
+    "url": "/React-Kamasutra/static/js/4.084f5000.chunk.js"
+  },
+  {
+    "revision": "745fe3f6a3da892eb39a",
+    "url": "/React-Kamasutra/static/js/5.9aa6d93f.chunk.js"
+  },
+  {
+    "revision": "50272ab09f0332368bf2",
+    "url": "/React-Kamasutra/static/js/main.f0e7007a.chunk.js"
+  },
+  {
+    "revision": "68ff055287e3f0dae35f",
+    "url": "/React-Kamasutra/static/js/runtime-main.79c0d202.js"
+  },
+  {
+    "revision": "9134df9cf1429fb69f39e7f932546070",
+    "url": "/React-Kamasutra/static/media/preloader.9134df9c.gif"
+  },
+  {
+    "revision": "0ec44ea334772f358c731f0a2ee2b74e",
+    "url": "/React-Kamasutra/static/media/user.0ec44ea3.webp"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Kamasutra/precache-manifest.77135d9a2dc572554d31422c095225cf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Kamasutra/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
